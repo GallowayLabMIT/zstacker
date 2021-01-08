88852e12af21627808b7b550c77bb93782ac46c1
Conrad Oakes <oakesconrad@gmail.com>
2021-01-07 16:09:24 -0800

    Testing turning tiff into fog
    Should take a tiff and create several fogs based off each channel

--- src/main.cpp
+++ src/main.cpp
@@ -6,9 +6,14 @@
 
 #define RESOLUTION 100
 
-int main()
+void makeTiffFog(TIFF* tif)
 {
-    TIFF* tif = TIFFOpen("test.tif", "w");
+    openvdb::FloatGrid::Ptr grid =
+        openvdb::FloatGrid::create(0);
+    grid->setTransform(
+        openvdb::math::Transform::createLinearTransform(/*voxel size=*/1.0));
+    grid->setGridClass(openvdb::GRID_FOG_VOLUME);
+    // Name the grid "LevelSetSphere".
     TIFFSetField(tif, TIFFTAG_IMAGEWIDTH, RESOLUTION);
     TIFFSetField(tif, TIFFTAG_IMAGELENGTH, RESOLUTION);
     TIFFSetField(tif, TIFFTAG_BITSPERSAMPLE, 8);
@@ -19,51 +24,73 @@ int main()
     TIFFSetField(tif, TIFFTAG_PHOTOMETRIC, PHOTOMETRIC_MINISBLACK);
     TIFFSetField(tif, TIFFTAG_SAMPLEFORMAT, SAMPLEFORMAT_UINT);
     TIFFSetField(tif, TIFFTAG_COMPRESSION, COMPRESSION_NONE);
-
-    uint8_t array[RESOLUTION];
-    for (uint8_t i = 0; i < RESOLUTION; ++i)
-    {
-        // Update the array
-        for (uint8_t j = 0; j < RESOLUTION; ++j)
-        {
-            array[j] = ((i + j) % RESOLUTION) * 2;
+    
+    uint32_t width, height;
+    TIFFGetField(tif, TIFFTAG_IMAGEWIDTH, &width);
+    TIFFGetField(tif, TIFFTAG_IMAGELENGTH, &height);
+    uint32_t * raster = static_cast<uint32_t*>(_TIFFmalloc(width * height * sizeof(uint32_t)));
+    TIFFReadRGBAImage(tif, width, height, raster, 0);
+    openvdb::FloatGrid::Accessor accessor= grid->getAccessor();
+    openvdb::Coord ijk;
+    int &i = ijk[0], &j = ijk[1], &k = ijk[2];
+    k = 1;
+//    for each coordinate, gets tiff pixel and sets fog depending on color
+    for (i = 0 - width; i < 0 + width; ++i){
+        for (j = 0 - height; j < 0 + height; ++j){
+            int R= TIFFGetR(raster[i*j]);
+            grid->setName("channelR");
+            if (R > 122.5){
+                accessor.setValue(ijk, 1);
+            }
+            else{
+                accessor.setValueOff(ijk, 0);
+            }
+            int G= TIFFGetG(raster[i*j]);
+            grid->setName("channelG");
+            if (G > 122.5){
+                accessor.setValue(ijk, 1);
+            }
+            else{
+                accessor.setValueOff(ijk, 0);
+            }
+            int B= TIFFGetB(raster[i*j]);
+            grid->setName("channelB");
+            if (B > 122.5){
+                accessor.setValue(ijk, 1);
+            }
+            else{
+                accessor.setValueOff(ijk, 0);
+            }
+            int A= TIFFGetA(raster[i*j]);
+            grid->setName("channelA");
+            if (A > 122.5){
+                accessor.setValue(ijk, 1);
+            }
+            else{
+                accessor.setValueOff(ijk, 0);
+            }
         }
-        TIFFWriteScanline(tif, array, i, 0);
     }
     
-    TIFFClose(tif);
-    std::cout << "Finished writing test TIFF\n";
-
-    openvdb::initialize();
-    // Create a shared pointer to a newly-allocated grid of a built-in type:
-    // in this case, a FloatGrid, which stores one single-precision floating point
-    // value per voxel.  Other built-in grid types include BoolGrid, DoubleGrid,
-    // Int32Grid and Vec3SGrid (see openvdb.h for the complete list).
-    // The grid comprises a sparse tree representation of voxel data,
-    // user-supplied metadata and a voxel space to world space transform,
-    // which defaults to the identity transform.
-    openvdb::FloatGrid::Ptr grid =
-        openvdb::FloatGrid::create(/*background value=*/2.0);
-    // Associate some metadata with the grid.
-    grid->insertMeta("radius", openvdb::FloatMetadata(50.0));
-    // Associate a scaling transform with the grid that sets the voxel size
-    // to 0.5 units in world space.
-    grid->setTransform(
-        openvdb::math::Transform::createLinearTransform(/*voxel size=*/0.5));
-    // Identify the grid as a level set.
-    grid->setGridClass(openvdb::GRID_LEVEL_SET);
-    // Name the grid "LevelSetSphere".
-    grid->setName("LevelSetSphere");
     // Create a VDB file object.
-    openvdb::io::File file("testgrid.vdb");
+    openvdb::io::File file("tiffgrid.vdb");
     // Add the grid pointer to a container.
     openvdb::GridPtrVec grids;
     grids.push_back(grid);
-
-    std::cout << "Finished creating OpenVDB grid\n";
     // Write out the contents of the container.
     file.write(grids);
     file.close();
-    std::cout << "Done writing OpenVDB grid to disk\n";
+    _TIFFfree(raster);
+}
+
+
+int main()
+{
+    openvdb::initialize();
+    
+    TIFF* tif = TIFFOpen("test.tif", "w");
+    makeTiffFog(tif);
+    
+    TIFFClose(tif);
     return 0;
 }
